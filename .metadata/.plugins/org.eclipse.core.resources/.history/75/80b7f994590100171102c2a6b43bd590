98924d5c15ec47e4b18ca69126e20fbad0501798
suneela <deweswar.sai@gmail.com>
2017-03-04 22:16:48 -0600

    importing project

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/75/80b7f994590100171102c2a6b43bd590
@@ -0,0 +1,9 @@
+
+public class ClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Git_Eclipse_Proj/src/ClassOne.java
@@ -0,0 +1,10 @@
+
+public class ClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ClassOne");
+
+	}
+
+}
